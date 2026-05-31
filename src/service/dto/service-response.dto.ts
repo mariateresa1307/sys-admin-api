1aa95007f5a3f3c194e719c17200c72dc224c5b2
mariateresa1307 <mariateresa1307@gmail.com>
2026-05-30 22:17:44 -0400

    service

---
+++ src/service/dto/service-response.dto.ts
@@ -0,0 +1,19 @@
+export class ServiceResponseDto {
+  _id: string;
+  id_netuno: string;
+  serial_ont: string;
+  id_circuito?: string;
+  vlan: number;
+  nombre_cliente?: string;
+  contrato: number;
+  nodoA:string;
+  nodoB:string;
+  nodoOLT:string;
+  diagramaRed:string; 
+  status:string;
+
+  
+}
+
+
+  
---
+++ src/service/entities/service.entity.ts
@@ -0,0 +1,53 @@
+import {
+  Entity,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ObjectId,
+} from 'typeorm';
+
+@Entity('service')
+export class Service {
+  @ObjectIdColumn()
+  _id!: ObjectId;
+
+  @Column({ unique: true })
+  id_netuno!: string;
+
+  @Column()
+  serial_ont!: string;
+
+  @Column()
+  id_circuito!:    string;
+
+  @Column({ nullable: true })
+  vlan?: number;
+
+  @Column()
+  nombre_cliente!: string;
+
+  @Column({ nullable: true })
+  contrato?: number;
+
+  @Column({ nullable: true })
+  nodoA?: string;
+
+  @Column({ nullable: true })
+  nodoB?: number;
+
+  @Column({ nullable: true })
+  nodoOLT?: number;
+
+  @Column({ nullable: true })
+  diagramaRed?: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @Column({ default: true })
+  status?: string;
+}
