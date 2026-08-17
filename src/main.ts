de4e70439ee2a356312b0d6940b3edac841a7670
mariateresa1307 <mariateresa1307@gmail.com>
2026-08-16 23:11:32 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,8 @@ app.use(express.json({ limit: '10mb' }));
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000', 'http://172.16.3.53:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
